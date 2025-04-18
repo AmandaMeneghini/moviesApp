2b1101b2c82687e693edbd593c1a74c517718292
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-18 06:30:40 -0300

    style(movies): refactor the Movies component to include styling and modal functionality

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,10 +1,63 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text, Image, TouchableOpacity, Modal, StyleSheet } from "react-native";
 
 export default function Movies({data}){
+
+    const [visibleModal, setVisibleModal] = useState(false);
+
     return(
         <View>
-            <Text>{data.nome}</Text>
+            <View style={styles.card}>
+                <Text style={styles.title}>{data.nome}</Text>
+                <Image 
+                source={{uri: data.foto}} 
+                style={styles.cover}/>
+
+                <View style={styles.btnArea}>
+                    <TouchableOpacity style={styles.btn} onPress={() =>{}}>
+                        <Text style={styles.btnText}>LEIA MAIS</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <Modal transparent={true} animationType="slide" visible={visibleModal}>
+                <Text>TESTE</Text>
+            </Modal>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        margin: 15,
+        elevation: 2,
+    },
+    cover: {
+        height: 250,
+        zIndex: 2,
+    },
+    title: {
+        padding: 15,
+        fontSize: 18,
+    },
+    btnArea: {
+        alignItems: 'flex-end',
+        marginTop: -40,
+        zIndex: 9,
+    },
+    btn: {
+        width: 100,
+        backgroundColor: '#09A6FF',
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+    },
+    btnText: {
+        color: '#fff',
+        textAlign:'center'
+    },
+
+})
