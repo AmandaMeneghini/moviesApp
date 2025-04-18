a0c7af242eccc21e58bfae142c9cee3ab63b26dc
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-18 07:27:29 -0300

    feat(app): adds loading indicator when fetching movie data

--- App.js
+++ App.js
@@ -1,39 +1,56 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 
 import api from './src/services/api';
 import Movies from './src/components/Movies';
 
 export default function App() {
-
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadMovies() {
       const response = await api.get('r-api/?api=filmes');
 
       setMovies(response.data);
+      setLoading(false);
     }
 
     loadMovies();
-
   }, []);
 
-  return(
-    <View styles={styles.container}>
-      
-      <FlatList
-        data={movies}
-        keyExtractor={item =>  String(item.id)}
-        renderItem={({item}) => <Movies data={item}/>}
-       />
-
-    </View>
-  );
+  if (loading) {
+    return (
+      <View style={styles.loadingView}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={movies}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => <Movies data={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
+  },
+  loadingView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
 });
